386127a854adf925af35a950883d985d499d1b75
antopatroni <apatroniturlo@mail.austral.edu.ar>
2025-04-04 13:01:53 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,20 @@
 def change():
-    expense = 23.75
-    money = 100
+    gasto = 23.75
+    recibido = 100
+
+    print("Ingresar gasto:")
+    print(gasto)
+    print("dinero recibido")
+    print(recibido)
+   # vuelto = pagado - gasto
+   # pesos = int(vuelto)
+   # centavos = 
+    vuelto = recibido - gasto
+    print("\nVuelto\n")
+    print("Pesos:")
+    print(int(vuelto))
+    print("Centavos:")
+    print(int((vuelto - int(vuelto)) * 100))
+
+
+change()
